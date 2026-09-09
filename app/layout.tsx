96ea10bbbe65ac5536d17199b6931ff713ff6ec9
Yuubbie <ubongemmanuel.james@gmail.com>
2026-09-09 10:38:04 +0100

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import IntroSplash from "@/components/IntroSplash";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 
 const fraunces = Fraunces({
   subsets: ["latin"],
@@ -33,6 +34,7 @@ export default function RootLayout({
       <body
         className={`${fraunces.variable} ${inter.variable} font-body bg-paper text-ink antialiased`}
       >
+        <GoogleAnalytics />
         <IntroSplash />
         <Navbar />
         {children}
